fff4795dc47e92e0d751ce384fecef57bf86a444
Steven Bowen <sdbowen.git@gmail.com>
2018-11-20 17:57:34 -0200

    Refactor routes to use resources and correct link_to

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # List all tasks
-  get 'tasks', to: 'tasks#index', as: 'tasks'
-
-  # New Task
-  get 'tasks/new', to: 'tasks#new', as: 'new'
-  
-  # Create task
-  post 'tasks', to: 'tasks#create'
-
-  # Get task
-  get 'tasks/:id', to: 'tasks#show', as: 'task'
-
-  # Edit task detail
-  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit'
-
-  # Update task
-  patch 'tasks/:id', to: 'tasks#update'
-
-  # Delete task
-  delete 'tasks/:id', to: 'tasks#destroy'
-
-
+  resources :tasks
 end
