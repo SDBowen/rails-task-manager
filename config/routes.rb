97ecb40492cd392a8ae1dd9b05062afe147e9bda
Steven Bowen <sdbowen.git@gmail.com>
2018-11-20 14:18:02 -0200

    Add individual task view

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # List tasks
+  # List all tasks
   get 'tasks', to: 'tasks#index'
-  
+
+  # Get task
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+
 
 end
